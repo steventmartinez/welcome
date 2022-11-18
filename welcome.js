5db4d6043db135c74159ae280d3b01927d554ba7
Steven Martinez <stevent.martinez@temple.edu>
2022-11-18 14:09:36 -0500

    first commit

---
+++ welcome.js
@@ -0,0 +1,48 @@
+//Create timeline
+var timeline = [];
+
+
+    /* init connection with pavlovia.org */
+    var pavlovia_init = {
+        type: jsPsychPavlovia,
+        command: "init"
+    };
+    timeline.push(pavlovia_init);
+
+
+///Welcome message and PID entry
+var welcome = {
+    type: jsPsychSurveyText,
+    questions: [{prompt: "<p><div style ='font-size:50px;'>Welcome to the experiment!</div></p>" +
+    "<p><div style ='font-size:50px;'>Please enter your participant ID.</div></p>", name: 'PID', required: true}],
+    data: {startdate: fulldate},
+    trial_duration: 0,
+    on_finish: function(data) {
+    // when this trial ends, check the data
+    //console.log('response: ', data.response);
+    var response = data.response;
+    console.log(response)
+    console.log(response.PID)
+    //console.log(response.PID)
+    id = Number(Object.values(response))
+    console.log(id)
+    jsPsych.data.addProperties({
+        PID: response.PID, 
+    })
+    PID_condition = id % 3
+    console.log(PID_condition)
+  },
+  }
+
+
+  timeline.push(welcome);
+
+
+
+  		/* finish connection with pavlovia.org */
+          var pavlovia_finish = {
+			type: jsPsychPavlovia,
+			command: "finish",
+			participantId: "JSPSYCH-DEMO"
+		};
+		timeline.push(pavlovia_finish);
